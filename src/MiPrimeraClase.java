6910ed11ee873913755f23867ae5e91e619d6e50
Lenovo <verastegui.g.mauricio@gmail.com>
2021-02-04 14:39:35 +0100

    Otra nuevisima version

--- src/MiPrimeraClase.java
+++ src/MiPrimeraClase.java
@@ -4,7 +4,7 @@ public class MiPrimeraClase {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		//Esto es una nueva versión
+		//Esto es una nueva puta versión
 	}
 
 }
